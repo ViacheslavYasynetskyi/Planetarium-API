664fa46069b0ae701b3739780725a8415ad73310
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-10-09 22:53:24 +0100

    add models in models.py & registration models in admin.py

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    AstronomyShow,
+    PlanetariumDome,
+    Reservation,
+    ShowTheme,
+    ShowSession,
+    Ticket,
+)
+
+admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
+admin.site.register(Reservation)
+admin.site.register(ShowTheme)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
